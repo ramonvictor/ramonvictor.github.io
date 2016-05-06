d97d59d826aff91c5c82ed9a6a97226d31a4428d
Ramon Bezerra <ramon.bezerra@booking.com>
2016-05-06 10:32:40 +0200

    remove angularjs-intervalo-cesar folder

--- angularjs-intervalo-cesar/test/po/cart.po.js
+++
@@ -1,8 +0,0 @@
-// Cart Page Object
-var CartPage = function(){
-  this.totalEl = element.all(by.binding('cart.getTotal()'));
-  this.cartItem = element.all(by.model('cartItem.quantity'));
-  this.testEl = element(by.css('table tr:nth-child(3)'));
-};
-
-module.exports = CartPage;
